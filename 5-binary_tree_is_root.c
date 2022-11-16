45647f83b57d7436b628438a7f54cd4249dc5d24
Ripeplantain <irvingmanny@gmail.com>
2022-11-16 02:51:31 +0000

    agian

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -7,5 +7,12 @@
  */
  int binary_tree_is_leaf(const binary_tree_t *node)
  {
-    if ()
+    if (node == NULL)
+    {
+        return (0);
+    }
+
+    if (!node->parent)
+        return (1);
+    return (0);    
  }

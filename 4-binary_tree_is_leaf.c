9d252090a6e2bf5c70c2ab9b7a12f103bbdfef1a
Ripeplantain <irvingmanny@gmail.com>
2022-11-16 02:45:18 +0000

    money money

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,10 +6,8 @@
  * Return: A pointer to to created node or NULL on failure.
  */
  int binary_tree_is_leaf(const binary_tree_t *node)
- {
-    if (node->left == NULL || node->right == NULL)
-    {
-        return (1);
-    }
-    return (0);
- }
+{
+	if (node && !node->left && !node->right)
+		return (1);
+	return (0);
+}

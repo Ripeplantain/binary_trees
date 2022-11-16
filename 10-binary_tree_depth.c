23fb458f135d3acfbaa5865cea79a9551770e8bf
Ripeplantain <irvingmanny@gmail.com>
2022-11-16 15:11:32 +0000

    stuff

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,19 +1,19 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_insert_left - A function that inserts a node as the left-child.
- * @parent: A pointer to a node to insert the left child in.
- * @value: The value to store in the node.
- * Return: A pointer to to created node or NULL on failure.
+ * binary_tree_depth - gets the depth of a node in abinary tree
+ * @tree: root node to draw height from for tree
+ *
+ * Return: size_t representing height, 0 on failure or NULL
  */
- size_t binary_tree_depth(const binary_tree_t *tree)
- {
-    size_t depth = 0;
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
+	if (!tree)
+		return (0);
 
-    depth += 1 + binary_tree_depth(tree->parent);
-      
- }
+	if (tree->parent)
+		depth += 1 + binary_tree_depth(tree->parent);
+	return (depth);
+}

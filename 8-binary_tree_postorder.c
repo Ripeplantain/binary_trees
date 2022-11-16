6bd2e30e902f5435aab7d9910bb98c6c3419f128
Ripeplantain <irvingmanny@gmail.com>
2022-11-16 15:07:33 +0000

    betty

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,17 +1,16 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_insert_left - A function that inserts a node as the left-child.
- * @parent: A pointer to a node to insert the left child in.
- * @value: The value to store in the node.
- * Return: A pointer to to created node or NULL on failure.
+ * binary_tree_postorder - traverses a tree in post-order calling func
+ * @tree: const pointer to root node to begin traversal at
+ * @func: function to call on tree node's data
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-    {
-        return; 
-    }
-    binary_tree_postorder(tree->right, func);
-    binary_tree_postorder(tree->left, func);
-    func(tree->n);
+	if (!tree || !func)
+		return;
+
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
 }

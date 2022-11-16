bee8afb51edb98e780a2a40bf4431a7f4adc9b66
Ripeplantain <irvingmanny@gmail.com>
2022-11-16 02:47:39 +0000

    stuff

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,7 +7,10 @@
  */
  int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node && !node->left && !node->right)
+	if (node == NULL)
+		return(0);
+
+	if (!node->left && !node->right)
 		return (1);
 	return (0);
 }

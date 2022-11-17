7d869207369ad3d4bdec33919c885843cdd4fd83
Rehema Msosa <105172957+Nyamso@users.noreply.github.com>
2022-11-17 22:10:36 +0200

    betty

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,19 +1,20 @@
-#include "binary_trees.h"
+include "binary_trees.h"
+
 /**
- * binary_tree_insert_left - A function that inserts a node as the left-child.
- * @parent: A pointer to a node to insert the left child in.
- * @value: The value to store in the node.
- * Return: A pointer to to created node or NULL on failure.
+ * binary_tree_size - Measures the size of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the size of.
+ *
+ * Return: The size of the tree.
  */
- size_t binary_tree_size(const binary_tree_t *tree)
- {
-    size_t size = 1;
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
 
-    if (!tree)
-        return (0);
-
-    size += binary_tree_size(tree->left);
-    size += binary_tree_size(tree->right);
-
-    return (size);    
- }
+	if (tree)
+	{
+		size += 1;
+		size += binary_tree_size(tree->left);
+		size += binary_tree_size(tree->right);
+	}
+	return (size);
+}

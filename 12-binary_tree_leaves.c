14727fa303c921371cede7613406a37f0fc2aeac
Rehema Msosa <105172957+Nyamso@users.noreply.github.com>
2022-11-17 22:18:21 +0200

    updated

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,22 +1,20 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_insert_left - A function that inserts a node as the left-child.
- * @parent: A pointer to a node to insert the left child in.
- * @value: The value to store in the node.
- * Return: A pointer to to created node or NULL on failure.
+ * binary_tree_leaves - Counts the leaves in a binary tree.
+ * @tree: A pointer to the root node of the tree to count the leaves of.
+ *
+ * Return: The number of leaves in the tree.
  */
- size_t binary_tree_leaves(const binary_tree_t *tree)
- {
-    size_t leaves = 0;
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
 
-    if (tree == NULL)
-        return (0);
-
-    if (!tree->left || !tree->right)
-        leaves += 1;
-
-    leaves += binary_tree_leaves(tree->left);        
-    leaves += binary_tree_leaves(tree->right);
-
-    return (leaves);        
- }
+	if (tree)
+	{
+		leaves += (!tree->left && !tree->right) ? 1 : 0;
+		leaves += binary_tree_leaves(tree->left);
+		leaves += binary_tree_leaves(tree->right);
+	}
+	return (leaves);
+}

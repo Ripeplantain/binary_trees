d78164076544292ffdb9118a0b3b4d6fe646d1e1
Rehema Msosa <105172957+Nyamso@users.noreply.github.com>
2022-11-17 21:57:17 +0200

    betty corrected

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,18 +1,15 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - A function that inserts a node as the left-child.
- * @parent: A pointer to a node to insert the left child in.
- * @value: The value to store in the node.
- * Return: A pointer to to created node or NULL on failure.
+ * binary_tree_is_root - Checks if a node is a root of a binary tree.
+ * @node: A pointer to the node to check.
+ *
+ * Return: If the node is a root - 1.
+ *         Otherwise - 0.
  */
  int binary_tree_is_root(const binary_tree_t *node)
- {
-    if (node == NULL)
-    {
-        return (0);
-    }
+{
+	if (node == NULL || node->parent != NULL)
+		return (0);
 
-    if (!node->parent)
-        return (1);
-    return (0);    
- }
+	return (1);
+}

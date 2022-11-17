c2708fae66d74eb6e10131290b99383ec8bc0b40
Rehema Msosa <105172957+Nyamso@users.noreply.github.com>
2022-11-17 21:52:07 +0200

    Corrected

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,9 +1,7 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - A function that inserts a node as the left-child.
- * @parent: A pointer to a node to insert the left child in.
- * @value: The value to store in the node.
- * Return: A pointer to to created node or NULL on failure.
+ * binary_tree_delete - Deletes a binary tree.
+ * @tree: A pointer to the root node of the tree to delete.
  */
 
 void binary_tree_delete(binary_tree_t *tree)
